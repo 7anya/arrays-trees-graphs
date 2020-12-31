eec615344b0c90810aff176be2b7841e6d561a7e
Tanya Prasad <55975635+maruyari@users.noreply.github.com>
2020-12-31 15:39:10 +0530

    Create trees.cpp

---
+++ trees.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define int long long int
+#define ld long double
+#define MOD 1000000007
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(var) (var).begin(), (var).end()
+#define MAXX 9000000000000000000
+#define pii pair<int,int>
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define vpi vector<pair<int,int> >
+#define vvpi vector<vector<pair<int,int> > >
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+  
+    return 0;
+}
